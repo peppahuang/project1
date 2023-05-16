dff8961f6e7ee66ad8c59e64171a9ea4b1a02aa6
peppahuang <102675864+peppahuang@users.noreply.github.com>
2023-05-16 16:01:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ print("hello world!")
 
 ## edit online or on other PC by other people
 print("another hello world!")
+
+## another edit but you did not pull before you eidt your local file
+print("New editing.")
